71f713ad82690efb463ac1a59bc526ee6f64c078
Votre nom <votre-email@example.com>
2024-07-23 00:28:31 +0200

    Maps 4 : Cluster + Filtre : Test 6

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,216 +1,66 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet'; // Correction de l'importation de Leaflet
+import 'leaflet.markercluster'; // Importation du plugin MarkerCluster
 import { HttpClient } from '@angular/common/http';
-import * as L from 'leaflet';
-import 'leaflet.markercluster';
-import { BottomBarComponent } from '../bottom-bar/bottom-bar.component';
 
 @Component({
-  selector: 'app-leaflet-carte',
+  selector: 'app-test',
   standalone: true,
-  imports: [BottomBarComponent],
-  templateUrl: './leaflet-carte.component.html',
-  styleUrls: ['./leaflet-carte.component.css']
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  @ViewChild('map', { static: true }) mapContainer?: ElementRef<HTMLDivElement>;
-  private map!: L.Map;
-  private markerClusterGroup!: L.MarkerClusterGroup;
-  private currentLocationMarker?: L.Marker;
-  private currentLocationCircle?: L.Circle;
-
-  private allMarkersData: any[] = [];
-  private selectedProjectFilters: Set<string> = new Set();
-  private selectedProductFilters: Set<string> = new Set();
-  private selectedTechniqueFilter: string = 'toutes';
-
-  public filteredFilters = [
-    { type: 'ferme-urbaine-participative', label: 'Ferme Urbaine Participative' },
-    { type: 'jardin-potager', label: 'Jardin Potager' },
-    { type: 'herbes-aromatiques', label: 'Herbes Aromatiques' },
-    { type: 'legumes', label: 'Légumes' },
-    { type: 'plants', label: 'Plants' }
-  ];
-
-  public totalLocations: number = 0;
+  private map: L.Map | undefined;
+  private markers: L.MarkerClusterGroup = L.markerClusterGroup(); // Initialiser avec une valeur par défaut
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.initMap();
     this.loadMarkers();
-    this.addCurrentLocationMarker();
   }
 
   private initMap(): void {
-    if (!this.mapContainer?.nativeElement) {
-      console.error('Element de la carte non trouvé.');
-      return;
+    this.map = L.map('map', {
+      center: [46.603354, 1.888334], // Centre approximatif de la France
+      zoom: 6,
+      layers: [
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '© OpenStreetMap contributors'
+        })
+      ]
+    });
+
+    // Ajout du groupe de clusters à la carte
+    if (this.map) {
+      this.map.addLayer(this.markers);
     }
-
-    this.map = L.map(this.mapContainer.nativeElement).setView([46.603354, 1.888334], 6);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(this.map);
-
-    this.markerClusterGroup = L.markerClusterGroup();
-    this.map.addLayer(this.markerClusterGroup);
   }
 
   private loadMarkers(): void {
-    this.http.get<any[]>('https://www.observatoire-agriculture-urbaine.org/json/listsites.php?v=1720789221209')
-      .subscribe({
-        next: data => {
-          if (data && Array.isArray(data) && data.length) {
-            this.allMarkersData = data;
-            this.updateMarkers();
-          } else {
-            console.warn('Aucune donnée de marqueur trouvée.');
-          }
-        },
-        error: error => {
-          console.error('Erreur lors du chargement des marqueurs', error);
-        }
-      });
-  }
+    this.http.get<any[]>('https://www.observatoire-agriculture-urbaine.org/json/listsites.php?v=1720789221209').subscribe(data => {
+      // Assurez-vous que this.markers n'est pas undefined avant de l'utiliser
+      if (this.markers) {
+        data.forEach(item => {
+          const lat = parseFloat(item.lat);
+          const lng = parseFloat(item.lng);
+          const title = item.title || 'No Title';
+          const iconUrl = item.img || 'https://example.com/default-icon.png'; // URL d'un icône par défaut
 
-  private updateMarkers(): void {
-    if (!this.markerClusterGroup) {
-      console.error('Groupe de cluster de marqueurs non initialisé.');
-      return;
-    }
+          const marker = L.marker([lat, lng], {
+            icon: L.icon({
+              iconUrl: iconUrl,
+              iconSize: [32, 32], // Taille de l'icône
+              iconAnchor: [16, 32], // Ancrage de l'icône
+              popupAnchor: [0, -32] // Ancrage de la popup
+            })
+          }).bindPopup(`<b>${title}</b>`);
 
-    this.markerClusterGroup.clearLayers();
-    const filteredData = this.allMarkersData.filter(site => this.isVisible(site));
-    this.totalLocations = filteredData.length;
-    filteredData.forEach(site => {
-      const lat = parseFloat(site.lat);
-      const lng = parseFloat(site.lng);
-      const title = site.title || 'Sans titre';
-      const img = site.img ? `<img src="${site.img}" alt="${title}" style="width:100px;height:auto;">` : '';
-
-      const popupContent = `
-        <div class="custom-popup">
-          <strong>${title}</strong><br>
-          ${img}<br>
-          <strong>Ville:</strong> ${site.ville}<br>
-          <strong>Code Postal:</strong> ${site.cp}<br>
-          <strong>Ouvert au public:</strong> ${site.ouvertpublic === '1' ? 'Oui' : 'Non'}<br>
-          <strong>Prix solidaires:</strong> ${site.prixsolidaires ? 'Oui' : 'Non'}<br>
-          <strong>HLM:</strong> ${site.hlm === '1' ? 'Oui' : 'Non'}<br>
-          <strong>QP:</strong> ${site.qpv === '1' ? 'Oui' : 'Non'}<br>
-          <strong>Type de projet:</strong> ${site.list_typeprojet.join(', ')}<br>
-          <strong>Activités:</strong> ${site.list_typeactivite.join(', ')}<br>
-          <strong>Techniques de production:</strong> ${site.list_techniqueprod.join(', ')}<br>
-          <strong>Types de production:</strong> ${site.list_typeprod.join(', ')}
-        </div>
-      `;
-
-      const marker = L.marker([lat, lng], {
-        icon: L.icon({
-          iconUrl: 'https://example.com/your-custom-marker-icon.png',
-          iconSize: [38, 38],
-          iconAnchor: [19, 38],
-          popupAnchor: [0, -38]
-        })
-      }).bindPopup(popupContent);
-
-      this.markerClusterGroup.addLayer(marker);
+          this.markers.addLayer(marker); // Ajout du marqueur au groupe de clusters
+        });
+      }
     });
-  }
-
-  private isVisible(site: any): boolean {
-    const projectTypes: string[] = site.list_typeprojet || [];
-    const productTypes: string[] = site.list_typeprod || [];
-    const techniqueTypes: string[] = site.list_techniqueprod || [];
-
-    const isProjectTypeVisible = this.selectedProjectFilters.size === 0 || projectTypes.some(type => this.selectedProjectFilters.has(type));
-    const isProductTypeVisible = this.selectedProductFilters.size === 0 || productTypes.some(type => this.selectedProductFilters.has(type));
-    const isTechniqueTypeVisible = this.selectedTechniqueFilter === 'toutes' || techniqueTypes.includes(this.selectedTechniqueFilter);
-
-    return isProjectTypeVisible && isProductTypeVisible && isTechniqueTypeVisible;
-  }
-
-  public onFilterChange(filterType: string, event: Event): void {
-    const selectElement = event.target as HTMLSelectElement;
-    console.log(`Filter Type: ${filterType}`);
-    console.log(`Selected Value: ${selectElement.value}`);
-
-    if (filterType === 'techniqueProduction') {
-      this.selectedTechniqueFilter = selectElement.value;
-    } else if (filterType === 'typeActivite') {
-      const value = selectElement.value;
-      if (value === 'toutes') {
-        this.selectedProjectFilters.clear();
-      } else {
-        if (selectElement.selectedOptions.length) {
-          this.selectedProjectFilters.add(value);
-        } else {
-          this.selectedProjectFilters.delete(value);
-        }
-      }
-    } else {
-      const value = selectElement.value;
-      if (selectElement.selectedOptions.length) {
-        this.selectedProductFilters.add(value);
-      } else {
-        this.selectedProductFilters.delete(value);
-      }
-    }
-
-    console.log('Selected Project Filters:', this.selectedProjectFilters);
-    console.log('Selected Product Filters:', this.selectedProductFilters);
-    console.log('Selected Technique Filter:', this.selectedTechniqueFilter);
-
-    this.updateMarkers();
-  }
-
-  public resetFilters(): void {
-    this.selectedProjectFilters.clear();
-    this.selectedProductFilters.clear();
-    this.selectedTechniqueFilter = 'toutes';
-    this.updateMarkers();
-  }
-
-  public applyFilters(): void {
-    this.updateMarkers();
-  }
-
-  private addCurrentLocationMarker(): void {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const { latitude, longitude } = position.coords;
-
-        if (this.currentLocationMarker) {
-          this.map.removeLayer(this.currentLocationMarker);
-        }
-
-        if (this.currentLocationCircle) {
-          this.map.removeLayer(this.currentLocationCircle);
-        }
-
-        this.currentLocationMarker = L.marker([latitude, longitude]).addTo(this.map)
-          .bindPopup('Vous êtes ici').openPopup();
-
-        this.currentLocationCircle = L.circle([latitude, longitude], {
-          color: 'blue',
-          fillColor: '#30f',
-          fillOpacity: 0.2,
-          radius: 500
-        }).addTo(this.map);
-
-        this.map.setView([latitude, longitude], 13);
-      });
-    } else {
-      console.error('La géolocalisation n\'est pas supportée par ce navigateur.');
-    }
-  }
-
-  public centerOnCurrentLocation(): void {
-    if (this.currentLocationMarker) {
-      this.map.setView(this.currentLocationMarker.getLatLng(), 13);
-    } else {
-      this.addCurrentLocationMarker();
-    }
   }
 }
