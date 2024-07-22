8ad71818bc26fa6b6bd494e1194465c2ce113c38
Votre nom <votre-email@example.com>
2024-07-22 21:14:46 +0200

    Maps 2 : Cluster

--- src/app/components/leaflet-carte/leaflet-carte.component.ts
+++ src/app/components/leaflet-carte/leaflet-carte.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet.markercluster';
 
 @Component({
   selector: 'app-leaflet-carte',
@@ -11,6 +12,8 @@ export class LeafletCarteComponent implements OnInit {
   @ViewChild('map', { static: true }) mapContainer!: ElementRef;
 
   private map!: L.Map;
+  private markers: L.Marker[] = [];
+  private markerClusterGroup!: L.MarkerClusterGroup;
 
   ngOnInit(): void {
     this.initMap();
@@ -23,8 +26,27 @@ export class LeafletCarteComponent implements OnInit {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.map);
 
-    L.marker([51.5, -0.09]).addTo(this.map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    // Initialize the marker cluster group
+    this.markerClusterGroup = L.markerClusterGroup();
+    this.map.addLayer(this.markerClusterGroup);
+
+    // Add some markers to the cluster group
+    this.addMarkers();
+  }
+
+  private addMarkers(): void {
+    const locations = [
+      { lat: 51.5, lng: -0.09 },
+      { lat: 51.51, lng: -0.1 },
+      { lat: 51.49, lng: -0.08 },
+      { lat: 51.48, lng: -0.1 },
+      { lat: 51.47, lng: -0.09 }
+    ];
+
+    locations.forEach(location => {
+      const marker = L.marker([location.lat, location.lng])
+        .bindPopup('Marker at [' + location.lat + ', ' + location.lng + ']');
+      this.markerClusterGroup.addLayer(marker);
+    });
   }
 }
