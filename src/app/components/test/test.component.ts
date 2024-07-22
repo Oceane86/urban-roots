7be3c6b9840641d6f7892c69f7bce595eb7c5622
Votre nom <votre-email@example.com>
2024-07-23 00:20:00 +0200

    Maps 4 : Cluster + Filtre : Test 6

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,48 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import L from 'leaflet';
-
-// @Component({
-//   selector: 'app-test',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './test.component.html',
-//   styleUrl: './test.component.css'
-// })
-// export class TestComponent implements OnInit {
-//     private map: L.Map | undefined;
-
-//     ngOnInit(): void {
-//       this.initMap();
-//     }
-
-//     private initMap(): void {
-//       this.map = L.map('map', {
-//         center: [51.505, -0.09],
-//         zoom: 13,
-//         layers: [
-//           L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//             maxZoom: 19,
-//             attribution: '© OpenStreetMap contributors'
-//           })
-//         ]
-//       });
-
-//       const markers = L.markerClusterGroup();
-
-//       for (let i = 0; i < 100; i++) {
-//         const marker = L.marker([
-//           51.5 + Math.random() * 0.1,
-//           -0.09 + Math.random() * 0.1
-//         ]);
-//         markers.addLayer(marker);
-//       }
-
-//       this.map.addLayer(markers);
-//     }
-
-// }
-
-
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
@@ -259,4 +214,3 @@ export class TestComponent implements OnInit {
     }
   }
 }
-
