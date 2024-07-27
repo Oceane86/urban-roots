b666eb914a9c610a0a6a2f272ef7b4a025422b23
Votre nom <votre-email@example.com>
2024-07-27 21:44:59 +0200

    Cluster Test 5

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import 'leaflet.markercluster';
 
 @Component({
   selector: 'app-map',
@@ -23,15 +22,11 @@ export class MapComponent implements OnInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
 
-    const markers = L.markerClusterGroup();
-
     for (let i = 0; i < 100; i++) {
       const lat = 51.505 + (Math.random() - 0.5) * 0.1;
       const lng = -0.09 + (Math.random() - 0.5) * 0.1;
       const marker = L.marker([lat, lng]);
-      markers.addLayer(marker);
+      marker.addTo(map);
     }
-
-    map.addLayer(markers);
   }
 }
